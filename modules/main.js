1f88c416aa449585071d95d889282b0f13cde8b5
Jose Ulloa <jose.ulloa@gorillalogic.com>
2020-10-28 17:36:35 -0600

    clean up

--- modules/main.js
+++ modules/main.js
@@ -2,16 +2,31 @@
 import { createFigma } from './figma.js'
 import { FILE_ID, TOKEN } from './secrets.js'
 
+/**
+ * Instance of the figma client using my access token.
+ */
 const figma = createFigma(TOKEN)
 
+/**
+ * Loading div element.
+ */
 var loading = document.getElementById('loading')
-var container = document.getElementById('image-container')
 
+/**
+ * Container div of the svg image to animate.
+ */
+var imageContainer = document.getElementById('image-container')
+
+/**
+ * Gets the image from figma and put the result inside the image container.
+ */
 const loadImage = async () => {
   let img = await figma.getImageSVG(FILE_ID, '0:1')
 
   loading.remove()
-  container.innerHTML = img
+  imageContainer.innerHTML = img
 }
 
+
+// init
 loadImage()
--- modules/secrets.js
+++ modules/secrets.js
@@ -1,2 +1,9 @@
-export const FILE_ID = 'w02kwmteMCNzlYlvtZTLdw'
+/**
+ * Secret token to interact with figma API.
+ */
 export const TOKEN = '130485-df2a1bc6-9ba4-426c-8131-b646b2f0fff1'
+
+/**
+ * Id of the file in figma I want to animate.
+ */
+export const FILE_ID = 'w02kwmteMCNzlYlvtZTLdw'
